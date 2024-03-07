fe397a01d982db811069adc72f12f961999049f7
William Whitehead <willclashtest@gmail.com>
2024-03-07 12:41:47 -0600

    add officer links

--- src/data/officers.js
+++ src/data/officers.js
@@ -1,13 +1,62 @@
 export const OfficerList = [
-  { name: 'James Hurd', image: 'src/assets/images/officers/james_hurd.jpg', role: 'Co-chair' },
-  { name: 'Alexis Ng', image: 'src/assets/images/officers/Ng Alexis - 2022 - DSC_0935.JPG', role: 'Co-chair' },
-  { name: 'Will Whitehead', image: 'src/assets/images/officers/will_whitehead.JPG', role: 'Logistics Chair & Webmaster' },
-  { name: 'Joshua Lee', image: 'src/assets/images/officers/joshua_lee.jpeg', role: 'Publicity Director' },
-  { name: 'Zoe Kulphongpatana', image: 'src/assets/images/officers/zoekulphong.jpg', role: 'HackKU Chair' },
-  { name: 'Alexis Gray', image: 'src/assets/images/officers/alexisgray.jpeg', role: 'Secretary' },
-  { name: 'Vivian Lara', image: 'src/assets/images/officers/vivian_lara.jpeg', role: 'Treasurer' },
-  { name: 'Dylan Kneidel', image: 'src/assets/images/officers/dylan_kneidel.jpg', role: 'Programming Club Co-Chair' },
-  { name: 'Delroy Wright', image: 'src/assets/images/officers/Delroy_Wright.jpg', role: 'Programming Club Co-Chair' },
-  { name: 'Andrew Huang', image: 'src/assets/images/officers/andrew_huang.jpg', role: 'ESC/Expo Representative' },
-  // ... add more officers here if needed
+  {
+    name: "James Hurd",
+    image: "src/assets/images/officers/james_hurd.jpg",
+    role: "Co-chair",
+    link: "https://www.linkedin.com/in/jameshurd2718/",
+  },
+  {
+    name: "Alexis Ng",
+    image: "src/assets/images/officers/Ng Alexis - 2022 - DSC_0935.JPG",
+    role: "Co-chair",
+    link: "https://www.linkedin.com/in/alexis-ng/",
+  },
+  {
+    name: "Joshua Lee",
+    image: "src/assets/images/officers/joshua_lee.jpeg",
+    role: "Publicity Director",
+    link: "https://www.linkedin.com/in/joshualee128/",
+  },
+  {
+    name: "Will Whitehead",
+    image: "src/assets/images/officers/will_whitehead.JPG",
+    role: "Logistics Chair & Webmaster",
+    link: "https://www.linkedin.com/in/willwhitehead122/",
+  },
+  {
+    name: "Zoe Kulphongpatana",
+    image: "src/assets/images/officers/zoekulphong.jpg",
+    role: "HackKU Chair",
+    link: "https://www.linkedin.com/in/kulphongpatana/",
+  },
+  {
+    name: "Alexis Gray",
+    image: "src/assets/images/officers/alexisgray.jpeg",
+    role: "Secretary",
+    link: "https://www.linkedin.com/in/alexisgray623/",
+  },
+  {
+    name: "Vivian Lara",
+    image: "src/assets/images/officers/vivian_lara.jpeg",
+    role: "Treasurer",
+    link: "https://www.linkedin.com/in/vi-lara/",
+  },
+  {
+    name: "Dylan Kneidel",
+    image: "src/assets/images/officers/dylan_kneidel.jpg",
+    role: "Programming Club Co-Chair",
+    link: "https://www.linkedin.com/in/dylan-kneidel/",
+  },
+  {
+    name: "Delroy Wright",
+    image: "src/assets/images/officers/Delroy_Wright.jpg",
+    role: "Programming Club Co-Chair",
+    link: "https://www.linkedin.com/in/delroy-wright-440b35210/",
+  },
+  {
+    name: "Andrew Huang",
+    image: "src/assets/images/officers/andrew_huang.jpg",
+    role: "ESC/Expo Representative",
+    link: "https://www.linkedin.com/in/andrew-l-huang/",
+  },
 ];
